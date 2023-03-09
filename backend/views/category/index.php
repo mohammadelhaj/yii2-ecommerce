8fe59847ade665b5757613ad98e1d2c46c2c3e7f
Mohammad ALhaj <mohammad.alhaj@isae.edu.lb>
2023-03-09 16:45:48 +0200

    add list view for category

--- backend/views/category/index.php
+++ backend/views/category/index.php
@@ -7,15 +7,30 @@ use yii\helpers\Html;
 <div style="border:1px solid red; float:right;">
     <?= Html::a('add category', ['category/add-category'], ['class' => 'btn btn-link logout text-decoration-none']) ?>
 </div>
-<div class="sub" style="border: 1px solid red;height: 100%;width: 100%;">
-
-    <div style="border:1px solid blue;width: 20%;height: 100%;">
-        <?php
-        foreach ($model as $model) : ?>
-            <span>
-                <?= $model->description ?>
-                </br>
-            </span>
+<div class="container">
+    <h1 class="text-center">Hello Index Category</h1>
+    <div class="list-group list-group-flush">
+        <?php foreach ($model as $model) : ?>
+            <a href="#" class="list-group-item list-group-item-action">
+                <div class="d-flex justify-content-between align-items-center">
+                    <div style="width: 85%;">
+                        <h5 class="mb-1"><?= $model->name ?></h5>
+                        <p class="mb-0"><?= $model->description ?></p>
+                    </div>
+                    <div class="text-danger d-flex align-items-center" style="font-size: 30px;">
+                        <a href="#" class="me-3" title="View">
+                            <i class="fa fa-eye"></i>
+                        </a>
+                        <a href="#" class="me-3" title="Edit">
+                            <i class="fa fa-pencil"></i>
+                        </a>
+                        <a href="#" title="Delete">
+                            <i class="fa fa-trash"></i>
+                        </a>
+                    </div>
+                </div>
+            </a>
         <?php endforeach; ?>
     </div>
 </div>
+
